a3ada1912545e3ea656b92d4a5119aec3fae0eeb
Lewis <lewishamer0@gmail.com>
2023-10-19 22:30:59 +0100

    change hero text

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,8 +5,8 @@ export const Hero = () => {
     return (
         <section className="hero-section">
             <hgroup className="hero-header">
-                <h1>Bespoke websites & hand-crafted UI</h1>
-                <p>Hi I'm Lewis, a front end developer from the UK. If you're looking to level up your online presence, feel free to get in touch to discuss your needs.</p>
+                <h1>Hey there!</h1>
+                <p>I'm Lewis, a front end developer from the UK. I am passionate about making bespoke websites and hand-crafted UI. If you're looking to shake up your online presence, feel free to get in touch to discuss your needs.</p>
                 <Button onclick={() => location.href = "#contact"}>Get in touch</Button>
             </hgroup>
 
