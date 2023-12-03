70380f21841edc7ee644ccae0e7eed7a08b5b705
Lewis Hamer <77765949+hamer0@users.noreply.github.com>
2023-12-03 18:15:46 +0000

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ export const Hero = () => {
         <section className="hero-section hero-section--responsive">
             <hgroup className="hero-header">
                 <h1>Hey there!</h1>
-                <p>I'm Lewis, a front end developer from the UK. I am passionate about making bespoke websites and hand-crafted UI. If you're looking to shake up your online presence, feel free to get in touch to discuss your needs.</p>
+                <p>I'm Lewis, a software developer from the UK. I am passionate about making UI, websites and other coding projects - you can check out some of what I have made down below. I'm always open for a chat so feel free to get in touch.</p>
                 <Button onclick={() => location.href = "#contact"}>Get in touch</Button>
             </hgroup>
 
